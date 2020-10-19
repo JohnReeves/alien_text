503557f846475fc099eec39b2c6e99346e638008
John Reeves <JohnReeves@users.noreply.github.com>
2020-10-19 07:15:15 +0100

    Update translate.py

--- translate.py
+++ translate.py
@@ -2,17 +2,25 @@ from itertools import permutations
 from random import randint
 from math import factorial
 
-words=open("text.txt").read()
+words = ""
 alien_words = ""
 
+with open('text.txt', 'r') as f:
+   words = f.read()
+
 for word in words.split(' '):
-    if len(word) > 3:
+    middle=""
+    # 13! is about 6 billion and breaks the vm
+    if len(word) > 3 and len(word) < 10:
        pickone = randint(1,factorial(len(word)-4))
        middle = list(permutations(word[1:-1]))[pickone]
        middle = "{}{}{}".format(word[0],''.join(middle),word[-1])
     else:
-       middle=word[::-1]
+       middle = word
 
     alien_words += middle + " "
 
 print(alien_words)
+
+with open('alien_output.txt','w') as f:
+  f.write(alien_words)
